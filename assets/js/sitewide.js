7df569242d79af0c52c17197f6851f0ba04f9abc
lukeoprey88 <45082690+lukeoprey88@users.noreply.github.com>
2018-11-16 21:13:38 +0000

    Add files via upload
    Batch add of new files to include two updates. http://lukeoprey.net/resources/av-browne/

--- assets/js/sitewide.js
+++ assets/js/sitewide.js
@@ -1,4 +1,6 @@
 
 $( "button" ).click(function() {
-  $(".main-navigation").toggleClass( "hide" );
+  $(".main-navigation").toggleClass( "hide" ); // show nav
+  $(".overlay").toggleClass( "hide" ); // show and hide overlay
+  $(".navButton").toggleClass( "close" ); // show and hide close styling
 });
